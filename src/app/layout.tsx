5258bce47151242e55cfcb5b4a33e0dd32cf0e21
te <te@>
2025-12-23 11:42:23 +0300

    feat: Добавлен Yandex.Metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.webvisor.org/metrika/tag_ww.js?id=105972743', 'ym');
+
+              ym(105972743, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body className={`${inter.className} ${inter.variable} antialiased bg-black text-white`}>
+        {/* Yandex.Metrika counter */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/105972743" style="position:absolute; left:-9999px;" alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+
         {children}
       </body>
     </html>
